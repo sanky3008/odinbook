d42d08843f58ff292e07b39d588dfd83064b0c7b
sanky3008 <phadnis.sankalp691@gmail.com>
2025-01-05 17:58:48 +0530

    finish user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.order(created_at: :desc)
   end
 
   def index
+    @users = User.all.order(created_at: :desc)
   end
 end
