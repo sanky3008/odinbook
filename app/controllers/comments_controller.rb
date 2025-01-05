f8d14e38264d75878cc4b76c0c42d03fe40eafcb
sanky3008 <phadnis.sankalp691@gmail.com>
2025-01-05 17:45:40 +0530

    finish comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,24 @@
 class CommentsController < ApplicationController
   def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
-  def show
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [ :show, :index ]
-  resources :posts, only: [ :index, :new, :create, :show ]
-  resources :comments, only: [ :new, :create, :show ]
+  resources :posts, only: [ :index, :new, :create, :show ] do
+    resources :comments, only: [ :new, :create ]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
